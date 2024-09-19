372bf8a74e097d28231d4b76cf5bf17d4ee79803
Pavel Balashov <pavel.balashov1@gmail.com>
2024-09-19 10:43:23 +0200

    chore(app-nest-1): remove config module root import from the signalr module

--- apps/app-nest-1/src/signalr/signalr.module.ts
+++ apps/app-nest-1/src/signalr/signalr.module.ts
@@ -1,16 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SignalrController } from './signalr.controller';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      // No need to import in other modules
-      isGlobal: true,
-      expandVariables: true,
-      // cache: true,
-    }),
-  ],
+  imports: [],
   controllers: [SignalrController],
 })
 export class SignalrModule {}
